ad337c3e6b68776795ed7119d3b56fca165726bc
Raheel Mansoor <raheelmnsr@gmail.com>
2023-08-19 22:31:34 +0500

    project updated

--- database.php
+++ database.php
@@ -41,18 +41,125 @@ class database{
                 return false;         
             }else{
                 $this->conn = true;
-                echo "<br>this is constructor function. Connection successful";
                 return true;
             }
         }
     }
 
+    public function insert(string $table,array $row){
+        if($this->checkTable($table)){
+            $col_names = implode(',',array_keys($row));
+            $col_values = implode("','",array_values($row));
+            $sql = "INSERT INTO $table ($col_names) VALUES ('$col_values');";
+            if( $this->mysqli->query($sql)){
+                array_push($this->result,$this->mysqli->insert_id);
+                return true;
+            }else{
+                array_push($this->result,$this->mysqli->error);
+                return false;                
+            }
+        }
+    }
+
+    public function update(string $table,array $data,string $where=null){
+        if($this->checkTable($table)){
+            $args = [];
+            foreach($data as $key => $value){
+                $args[]= "$key = '$value'";
+            }
+            $str = implode(',',$args);
+            $sql = "UPDATE $table SET $str";
+            if($where != null){
+                $sql .= " WHERE $where ;";
+            }
+            if( $this->mysqli->query($sql)){
+                array_push($this->result,$this->mysqli->affected_rows);
+                return true;
+            }else{
+                array_push($this->result,$this->mysqli->error);
+                return false;                
+            }
+        }
+    } 
+
+    public function delete(string $table,string $where=NULL){
+        if($this->checkTable($table)){
+
+            $sql = "DELETE FROM $table";
+
+            if($where != null){
+                $sql .= " WHERE $where ;";
+            }            
+            if( $this->mysqli->query($sql)){
+                array_push($this->result,$this->mysqli->affected_rows);
+                return true;
+            }else{
+                array_push($this->result,$this->mysqli->error);
+                return false;                
+            }
+        }
+    } 
+    
+    public function sql(string $sql){
+          $records= $this->mysqli->query($sql);
+        if($records){
+            $this->result = $records->fetch_all(MYSQLI_ASSOC);
+            return true;
+        }else{
+            array_push($this->result,$this->mysqli->error);
+            return false;                
+        }
+    }      
+
+    public function select(string $table,string $rows = "*",string $join = null, string $where = null, string $orderby =NULL, int $limit=NULL){
+
+        if($this->checkTable($table)){
+            $sql = "SELECT $rows FROM $table";
+            if($join != NULL){
+                $sql .= " JOIN  $join";
+            }
+            if($where != NULL){
+                $sql .= " WHERE $where";
+            }
+            if($orderby != NULL){
+                $sql .= " ORDER BY $orderby";
+            }
+            if($limit != NULL){
+                $sql .= " LIMIT 0, $limit";
+            }
+            $records= $this->mysqli->query($sql);
+            if($records){
+                $this->result = $records->fetch_all(MYSQLI_ASSOC);
+                return true;
+            }else{
+                array_push($this->result,$this->mysqli->error);
+                return false;                
+            }
+        }else return false;
+    }  
+
+    public function getResults():array{
+        $val = $this->result;
+        $this->result = array();
+        return $val;
+    }
+    private function checkTable(string $table):bool {
+        $sql = "SHOW TABLES FROM {$this->db_name} LIKE '$table'";
+        $result = $this->mysqli->query($sql);
+        if($result){
+            if($result->num_rows>0){
+                return true;
+            }else{
+                array_push($this->result,$table." doesnot exists");
+                return false;
+            }
+        }
+    }
     //close db connection
     function __destruct(){
         if($this->conn){
             $this->mysqli->close();
             $this->conn = false;
-            echo "<br>this is destructor function. Connection close";
         }
             
     }
--- index.php
+++ index.php
@@ -6,6 +6,15 @@ require_once 'database.php';
 
 //create database object
 $obj = new database();
+// $obj->insert('students',['st_name'=>'akbar','st_age'=>32,'st_city'=>'peshawar']);
+// $obj->update('students',['st_name'=>'amir','st_age'=>22,'st_city'=>'peshawar'],"st_id = 6");
+// $obj->delete('students',"st_id = 6");
+// $obj->sql("SELECT * FROM students");
+$obj->select('students',"*",NULL,"st_city = 'peshawar'",NULL,1);
+$ans = $obj->getResults();
+echo "<pre>";
+print_r($ans);
+echo "</pre>";
 ?>
 
 
